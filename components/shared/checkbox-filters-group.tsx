e28c96f69840a596d72c1ab5fd564dacaaed02a4
max <Zeptorqyiad99@gmail.com>
2026-05-12 20:56:19 +0300

    update filter group

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import React from "react"
 import { FilterChecboxProps } from "./filter-checkbox"
+import { FilterCheckbox } from "@/components/shared"
+import { Input } from "../ui"
 
 type Item = FilterChecboxProps
 
@@ -8,7 +12,7 @@ interface Props {
     items: Item[]
     defaultItems?: Item[]
     limit?: number
-    serchInputPlaceholder?: string
+    searchInputPlaceholder?: string
     onChange?: (values: string[]) => void
     defaultValue?: string[]
     className?: string
@@ -19,10 +23,34 @@ export const CheckboxFilterGroup: React.FC<Props> = ({
     items,
     defaultItems,
     limit = 5,
-    serchInputPlaceholder = "Поиск...",
+    searchInputPlaceholder = "Поиск...",
     className,
     onChange,
     defaultValue,
 }) => {
-    return <div className={className}></div>
+    return (
+        <div className={className}>
+            <p className="font-bold mb-3">{title}</p>
+
+            <div className="mb-5">
+                <Input
+                    placeholder={searchInputPlaceholder}
+                    className="bg-gray-50 border-none"
+                />
+            </div>
+
+            <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+                {items.map((item, index) => (
+                    <FilterCheckbox
+                        onCheckedChange={(ids) => console.log(ids)}
+                        checked={false}
+                        key={index}
+                        value={item.value}
+                        text={item.text}
+                        endAdornment={item.endAdornment}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,11 @@
 import React from "react"
-import { FilterCheckbox, RangeSlider, Title } from "@/components/shared"
-import { Input } from "../ui";
+import {
+    FilterCheckbox,
+    RangeSlider,
+    Title,
+    CheckboxFilterGroup,
+} from "@/components/shared"
+import { Input } from "../ui"
 
 interface Props {
     className?: string
@@ -25,14 +30,57 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
                 <p className="font-bold mb-3">Цена от и до:</p>
                 <div className="flex gap-3 mb-5">
-                    <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
-                    <Input type="number" placeholder="1000" min={100} max={1000} />
+                    <Input
+                        type="number"
+                        placeholder="0"
+                        min={0}
+                        max={1000}
+                        defaultValue={0}
+                    />
+                    <Input
+                        type="number"
+                        placeholder="1000"
+                        min={100}
+                        max={1000}
+                    />
                 </div>
 
                 <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
             </div>
 
-            {}
+            <CheckboxFilterGroup
+                title="Ингридиенты"
+                className="mt-5"
+                limit={6}
+                defaultItems={[
+                    {
+                        text: "Сырный соус",
+                        value: "1",
+                    },
+                    {
+                        text: "Моцарелла",
+                        value: "2",
+                    },
+                    {
+                        text: "Чеснок",
+                        value: "3",
+                    },
+                ]}
+                items={[
+                    {
+                        text: "Сырный соус",
+                        value: "1",
+                    },
+                    {
+                        text: "Моцарелла",
+                        value: "2",
+                    },
+                    {
+                        text: "Чеснок",
+                        value: "3",
+                    },
+                ]}
+            />
         </div>
     )
 }
